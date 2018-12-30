c7f36a912ea269f0d61b4e570563d4e045f56be4
USER <user@USER-no-MacBook-Air-2.local>
2018-12-30 22:37:40 +0900

    exam/showの閲覧制限

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,4 +1,7 @@
 class ExamsController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  
+  
   def show
     @exam = Exam.offset( rand(Exam.count) ).first
     @review = current_user.reviews.find_by(exam_id: @exam.id)
