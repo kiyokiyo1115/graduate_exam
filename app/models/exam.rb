f4ceb0a3a9f49d6fa0c900c73927cd2328e98616
USER <user@USER-no-MacBook-Air-2.local>
2018-12-30 22:20:31 +0900

    日本語化およびexamモデルの誤字修正

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,4 +1,4 @@
 class Exam < ApplicationRecord
     has_many :reviews, dependent: :destroy
-    has_many :favorite_users, through: :favorites, source: :user  
+    has_many :review_users, through: :reviews, source: :user  
 end
