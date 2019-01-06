957e67c99cf24e75438cd7d6ede619e9bbb3487b
USER <user@USER-no-MacBook-Air-2.local>
2019-01-06 14:28:17 +0900

    reviews_controllerおよびexam.rbのインデント修正

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,4 +1,4 @@
 class Exam < ApplicationRecord
-    has_many :reviews, dependent: :destroy
-    has_many :review_users, through: :reviews, source: :user  
+  has_many :reviews, dependent: :destroy
+  has_many :review_users, through: :reviews, source: :user  
 end
